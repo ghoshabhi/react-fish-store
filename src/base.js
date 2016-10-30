36e059dacea97320b43f9fe4add3c786b00700ca
ghoshabhi <abhighosh_18@yahoo.com>
2016-10-30 12:51:17 -0700

    Init: A sample Fish Store made with React

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBl_IGEVqjggH7rW9p6ONUHfvGSXxKLek8",
+  authDomain: "catch-of-the-day-abhi.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-abhi.firebaseio.com"
+});
+
+export default base;
